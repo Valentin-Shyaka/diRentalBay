8e99c7509419d1f1b88e0bfe4b4745501d60e8f6
Valentin-Shyaka <vava1lantern7@gmail.com>
2024-08-22 13:15:35 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex bg-primary-white w-full h-screen ">
+      <div className="w-[70%] gap-3 h-full p-6 flex items-center justify-center  ">
+           
+          <div className="text-center">
+            <p className="text-black font-bold"> Welcome to <span className="text-green-500">RentalBay</span>  👋</p>
+            <p className="text-primary-gray">Are you a real estate owner or a property tenant ?</p>
+
+            <div className="p-4 border border-sm border-primary-green rounded-sm mt-4">
+
+            </div>
+          </div>
+           
+
+
+        
+        
+        
+        
+      </div>
+      <div className="bg-primary-gray w-full">
+
+      </div>
+    </div>
+  );
+}
